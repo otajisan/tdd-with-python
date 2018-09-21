31c8dca58ca1bf2e2678f156390f0c56d66da690
otajisan <taji.taji.otajisan@gmail.com>
2018-09-21 16:46:23 +0900

    :+1: append doctest sample

---
+++ otajisan/samples/tdd/play/use_doctest.py
@@ -0,0 +1,12 @@
+'''
+sample using doctest_namespace
+'''
+
+
+def add(a, b):
+    '''
+    >>> import otajisan.samples.tdd.play.use_doctest as ud
+    >>> ud.add(10, 3)
+    13
+    '''
+    return a + b
